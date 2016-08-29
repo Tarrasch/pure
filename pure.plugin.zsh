06e1396bda62787cfd4967a8468870b4a86cd154
Arash Rouhani <arashrk@vng.com.vn>
2016-08-29 15:10:26 +0700

    Expose no functions

--- pure.plugin.zsh
+++ pure.plugin.zsh
@@ -19,7 +19,7 @@
 
 # turns seconds into human readable time
 # 165392 => 1d 21h 56m 32s
-prompt_pure_human_time() {
+_pure_human_time() {
 	local tmp=$1
 	local days=$(( tmp / 60 / 60 / 24 ))
 	local hours=$(( tmp / 60 / 60 % 24 ))
@@ -32,7 +32,7 @@ prompt_pure_human_time() {
 }
 
 # fastest possible way to check if repo is dirty
-prompt_pure_git_dirty() {
+_pure_git_dirty() {
 	# check if we're in a git repo
 	command git rev-parse --is-inside-work-tree &>/dev/null || return
 	# check if it's dirty
@@ -42,14 +42,14 @@ prompt_pure_git_dirty() {
 }
 
 # displays the exec time of the last command if set threshold was exceeded
-prompt_pure_cmd_exec_time() {
+_pure_cmd_exec_time() {
 	local stop=$EPOCHSECONDS
 	local start=${cmd_timestamp:-$stop}
 	integer elapsed=$stop-$start
-	(($elapsed > ${PURE_CMD_MAX_EXEC_TIME:=5})) && prompt_pure_human_time $elapsed
+	(($elapsed > ${PURE_CMD_MAX_EXEC_TIME:=5})) && _pure_human_time $elapsed
 }
 
-prompt_pure_preexec() {
+_pure_preexec() {
 	cmd_timestamp=$EPOCHSECONDS
 
 	# shows the current dir and executed command in the title when a process is active
@@ -58,14 +58,14 @@ prompt_pure_preexec() {
 	print -Pn "\a"
 }
 
-prompt_pure_precmd() {
+_pure_precmd() {
 	# shows the full path in the title
 	print -Pn '\e]0;%~\a'
 
 	# git info
 	vcs_info
 
-	local prompt_pure_preprompt="\n%F{blue}%~%F{242}$vcs_info_msg_0_`prompt_pure_git_dirty` $prompt_pure_username%f %F{yellow}`prompt_pure_cmd_exec_time`%f"
+	local prompt_pure_preprompt="\n%F{blue}%~%F{242}$vcs_info_msg_0_`_pure_git_dirty` $prompt_pure_username%f %F{yellow}`_pure_cmd_exec_time`%f"
 	print -P $prompt_pure_preprompt
 
 	# reset value since `preexec` isn't always triggered
@@ -80,8 +80,8 @@ prompt_pure_precmd() {
 	autoload -Uz add-zsh-hook
 	autoload -Uz vcs_info
 
-	add-zsh-hook precmd prompt_pure_precmd
-	add-zsh-hook preexec prompt_pure_preexec
+	add-zsh-hook precmd _pure_precmd
+	add-zsh-hook preexec _pure_preexec
 
 	zstyle ':vcs_info:*' enable git
 	zstyle ':vcs_info:git*' formats ' %b'
